4d16b29a5f27022addc053b062fa56ddfa942614
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2023-05-19 14:26:14 -0400

    Update miner.py

--- miner.py
+++ miner.py
@@ -1,4 +1,5 @@
 import bitcoin_miner
+import getpass
 
 def mine_bitcoin():
     while True:
@@ -6,6 +7,14 @@ def mine_bitcoin():
         transaction_hash = bitcoin_miner.calculate_hash(transaction)
         
         if transaction_hash.startswith("0000"):  # Check if the hash meets the mining criteria
+            private_key = getpass.getpass("Enter your private key: ")
+            wallet_address = input("Enter your wallet address: ")
+            send_bitcoin(private_key, wallet_address)
             return transaction_hash
 
+def send_bitcoin(private_key, wallet_address):
+    # Add code here to send the mined Bitcoin to the provided wallet address using the private key.
+    # Note that the implementation details depend on the Bitcoin wallet software or service being used.
+
 mine_bitcoin()
+
